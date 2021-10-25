72e7a2c02bb7b871f5dcc7f31ddacdadbc0ace22
Antonio <antonioolvera1995@gmail.com>
2021-10-25 23:05:15 +0200

    init

---
+++ producer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Confluent.Kafka;
+namespace producer
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                sendMessage(message);
+            }
+
+
+        }
+
+        static ProducerConfig config()
+        {
+            return new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+            };
+        }
+
+
+        static void sendMessage(string message)
+        {
+
+            using (var producer = new ProducerBuilder<Null, string>(config()).Build())
+            {
+                var t = producer.ProduceAsync("topic-a", new Message<Null, string> { Value = message });
+                t.ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Wrote to offset: {task.Result.Offset}");
+                    }
+                });
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+
+            }
+
+        }
+
+    }
+
+}
